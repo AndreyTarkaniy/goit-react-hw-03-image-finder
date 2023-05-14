a468f889615f7512196c8f69e67a88954badad55
AndreyTarkaniy <endrubass@gmail.com>
2023-05-14 22:01:23 +0300

    add files components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,12 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
     </div>
   );
 };
+
+// API key: 34996310-6eb230e6525d45c07c1c5f00a
