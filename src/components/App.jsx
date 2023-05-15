31641fc6c28db9786c33d11ebab08e18cead5dfb
AndreyTarkaniy <endrubass@gmail.com>
2023-05-15 20:53:43 +0300

    add to loadmore button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,23 +3,26 @@ import * as ImageService from 'service/image-service';
 import { SearchBar } from 'components/searchbar/searchbar';
 import { ImageGallery } from 'components/imageGallery/imageGallery';
 import { ImageGalleryItem } from 'components/imageGalleryItem/imageGalleryItem';
+import { Button } from 'components/button/button';
 
 export class App extends Component {
   state = {
     query: '',
     page: 1,
     images: [],
+    totalImages: 0,
   };
 
   async componentDidUpdate(_, prevState) {
     const { query, page } = this.state;
 
-    if (prevState.query !== query) {
+    if (prevState.query !== query || prevState.page !== page) {
       const data = await ImageService.getImage(query, page);
-      console.log(data.data.hits);
+      console.log(data);
       this.setState(prevState => {
         return {
           images: [...prevState.images, ...data.data.hits],
+          totalImages: data.data.total,
         };
       });
     }
@@ -33,15 +36,26 @@ export class App extends Component {
     });
   };
 
-  render() {
-    const { images } = this.state;
+  incrementPage = () => {
+    this.setState(prevState => {
+      return {
+        page: prevState.page + 1,
+      };
+    });
+  };
 
+  render() {
+    const { images, totalImages } = this.state;
+
+    const showButton = images.length !== totalImages;
+    console.log(images.length);
     return (
       <div>
         <SearchBar onSubmit={this.getQuery} />
         <ImageGallery>
           <ImageGalleryItem images={images} />
         </ImageGallery>
+        {showButton && <Button onClick={this.incrementPage} />}
       </div>
     );
   }
